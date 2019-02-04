656eecbe1dfa26c5240e02d0230af15cb139cd68
Kevin Benevides <39235256801@info.local>
2019-02-04 11:29:53 -0200

    att

---
+++ SQL/HROADS_MANIPULACAO_02.sql
@@ -0,0 +1,29 @@
+INSERT INTO TIPO_HABILIDADES
+VALUES('Ataque'),('Defesa'),('Cura'),('Magia');
+
+INSERT INTO CLASSES
+VALUES('Bárbaro')
+	 ,('Cruzado')
+	 ,('Caçadora de Demônios')
+	 ,('Monge')
+	 ,('Necromante')
+	 ,('Feiticeiro')
+	 ,('Arcanista')
+
+INSERT INTO HABILIDADES
+VALUES('Lança Mortal',1)
+	 ,('Escudo Supremo',2)
+	 ,('Recuperar Vida',3)
+
+INSERT INTO CLASSES_HABILIDADES
+VALUES(1,1)
+	 ,(1,2)
+	 ,(2,2)
+	 ,(3,2)
+	 ,(3,3)
+	 ,(6,3)
+
+INSERT INTO PERSONAGENS
+VALUES('DeuBug',100,80,1,18/01/2019,04/02/2019)
+	 ,('BitBug',70,100,4,17/03/2016,04/02/2019)
+	 ,('Fer8',75,60,7,18/03/2018,04/02/2019)
